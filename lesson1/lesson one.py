6af933dabe2d814a25c489bced118a9f47944e42
Kyrylo Masunov <kim.od.ua@gmail.com>
2023-04-19 16:20:14 +0300

    apples and puplis

--- lesson1/lesson one.py
+++ lesson1/lesson one.py
@@ -1,4 +1,13 @@
-print('Hello World!',56, 76, 44.3, True)
+#print('Hello World!',56, 76, 44.3, True)
 
-a=45
-print(a)
+#a=45
+#print(a)
+
+#x =  input('Please input a value: ')
+
+#print(x, type(x))
+#y = int(x)
+#print(y, type(y))
+
+#string = 'Hello "World!"'
+#print(string)
---
+++ lesson1/operator.py
@@ -0,0 +1,4 @@
+print(5/6, type(5/6))
+print(10//2, type(10//2))
+print(5//6, type(5//6))
+print(3%1876, type(3%1876))
