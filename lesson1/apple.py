58afd2ab6159f242ef51c71cbf415157f894706b
Kyrylo Masunov <kim.od.ua@gmail.com>
2023-04-19 16:17:13 +0300

    apples and puples

---
+++ lesson1/apple.py
@@ -0,0 +1,10 @@
+# n -кільуість учнів к - кільуість яблок  в кошику
+x = input('Please enter number of students:')
+y = input('Please enter number of apple:')
+n = int(x)
+k = int(y)
+a = k//n
+b = k-a*n
+print(' яблок у кожного учня', a, 'apple')
+print (b,'aяблок залишилось в кошику')
+
